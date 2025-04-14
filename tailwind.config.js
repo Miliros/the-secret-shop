229f9b848201730ac58d848d19ce3f57efd0800c
Miliros <milagros4046@hotmail.com>
2025-04-14 11:17:16 -0300

    Initial commit: project structure and setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        retroGold: "#C6A664",
+        retroBlack: "#1C1C1C",
+        retroCream: "#F4F4F2",
+      },
+    },
+  },
+  plugins: [],
+};
