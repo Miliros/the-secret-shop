635fce6f82637244b4deedd14a59bfd7a2e0235f
Miliros <milagros4046@hotmail.com>
2025-04-15 17:34:20 -0300

    component footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { CartProvider } from "@/components/cart/CartContext";
+import Footer from "@/components/Footer"; // Asegúrate de importar tu componente Footer
 
 export const metadata: Metadata = {
   title: "The Secret Shop",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <CartProvider>
           <main>{children}</main>
         </CartProvider>
+        <Footer /> {/* Aquí colocas el Footer */}
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow"></main>
+
+      <footer className="bg-black text-white py-4">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2025 Old School Tech. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
