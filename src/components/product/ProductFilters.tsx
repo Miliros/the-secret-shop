8db7753c578ce60f2434f5508a03b2010d977da0
Miliros <milagros4046@hotmail.com>
2025-04-15 21:18:42 -0300

    add all filter options

--- src/components/product/ProductFilters.tsx
+++ src/components/product/ProductFilters.tsx
@@ -32,6 +32,21 @@ const ProductFilters: React.FC<ProductFiltersProps> = ({
           <option value="Audio" className="bg-white hover:text-white">
             Audio
           </option>
+          <option value="Fotografía" className="bg-white hover:text-white">
+            Fotografía
+          </option>
+          <option
+            value="Electrodomésticos"
+            className="bg-white hover:text-white"
+          >
+            Electrodomésticos
+          </option>
+          <option value="Telefonía" className="bg-white hover:text-white">
+            Telefonía
+          </option>
+          <option value="Oficina" className="bg-white hover:text-white">
+            Oficina
+          </option>
         </select>
       </div>
 
@@ -45,25 +60,26 @@ const ProductFilters: React.FC<ProductFiltersProps> = ({
         <select
           id="price-select"
           className="block w-full p-2 mb-6 text-sm text-gray-900 border border-black rounded-lg bg-gray-50 focus:ring-black focus:border-black dark:bg-black dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-black dark:focus:border-black sm:text-base"
-          onChange={(e) => {
-            const value = e.target.value;
-            if (value === "low") onPriceRangeChange([0, 50]);
-            else if (value === "mid") onPriceRangeChange([51, 200]);
-            else if (value === "high") onPriceRangeChange([201, Infinity]);
-            else onPriceRangeChange(null);
-          }}
+          onChange={(e) =>
+            onPriceRangeChange(
+              e.target.value ? JSON.parse(e.target.value) : null
+            )
+          }
         >
           <option value="" className="bg-white hover:text-white">
-            Todos
+            Todos los precios
           </option>
-          <option value="low" className="bg-white hover:text-white">
-            0 - 50
+          <option value="[0,50]" className="bg-white hover:text-white">
+            $0 - $50
           </option>
-          <option value="mid" className="bg-white hover:text-white">
-            51 - 200
+          <option value="[50,100]" className="bg-white hover:text-white">
+            $50 - $100
           </option>
-          <option value="high" className="bg-white  hover:text-white">
-            200+
+          <option value="[100,200]" className="bg-white hover:text-white">
+            $100 - $200
+          </option>
+          <option value="[200,500]" className="bg-white hover:text-white">
+            $200 - $500
           </option>
         </select>
       </div>
